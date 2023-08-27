074faf35e95d6d326b64a8a5a45c2ff99488389c
vishakjay <107393021+vishakjay@users.noreply.github.com>
2023-08-27 12:46:39 +0530

    Update 1230-Modex.cpp

--- 1230-Modex.cpp
+++ 1230-Modex.cpp
@@ -1,1 +1,26 @@
+  #include<bits/stdc++.h>
+  using namespace std;
+  long long binpow_mod(long long x,long long y,long long n){
+     long long res=1;  
+     //x%=n;
+    while(y){
+    if(y%2==1){
+        res=(res*x) % n ; 
+     }
+     x=(x*x) % n;
+     y/=2;
+    }
+    return res;
+  }
+  int main(){
 
+  int t;
+  cin>>t;
+  while(t--){
+    long long x,y,n;
+    cin>>x>>y>>n;
+    long long ans=binpow_mod(x,y,n);
+   cout<<ans<<endl;
+  }
+    return 0;
+  }
