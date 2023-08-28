d5f85faafdaa12357a387453a016293676e39328
vishakjay <107393021+vishakjay@users.noreply.github.com>
2023-08-29 01:40:00 +0530

    Update 10071-highSchoolPhysics.cpp

--- 10071-highSchoolPhysics.cpp
+++ 10071-highSchoolPhysics.cpp
@@ -1,1 +1,16 @@
 
+        #include <bits/stdc++.h>
+        using namespace std;
+        typedef long long ll;
+         
+        int main(){
+           
+         ll v,t,d; //int works as well
+         while( cin>>v>>t){
+        
+         d=v*2*t;
+         cout<<d<<endl;
+     }
+
+        return 0;
+        }
